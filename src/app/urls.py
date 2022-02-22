263157c558be3fcbca77feb0eee221f88a2a0e29
Leandro de Souza <leandrodesouzadev@gmail.com>
2022-02-22 09:52:09 -0300

    Feature (auth): Adds json-web-token authentication

--- src/app/urls.py
+++ src/app/urls.py
@@ -5,6 +5,8 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
+from app.base.jwt_auth import DecoratedTokenObtainPairView, DecoratedTokenRefreshView
+
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -22,6 +24,12 @@ urlpatterns = [
     path("", include("rest_framework.urls", namespace="rest_framework")),
     path("", include("users.urls")),
     path("docs/", schema_view.with_ui(), name="swagger"),
+    path(
+        "api/token/", DecoratedTokenObtainPairView.as_view(), name="token_obtain_pair"
+    ),
+    path(
+        "api/token/refresh/", DecoratedTokenRefreshView.as_view(), name="token_refresh"
+    ),
 ]
 
 if settings.DEBUG:  # pragma: no cover
