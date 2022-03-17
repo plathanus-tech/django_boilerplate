7cc4953c8f6e26de622ab7bc464daab5bddd34b4
Leandro de Souza <leandrodesouzadev@gmail.com>
2022-03-17 09:31:35 -0300

    Fix (users.apps): Removes default_auto_field from UsersConfig

--- src/users/apps.py
+++ src/users/apps.py
@@ -4,7 +4,6 @@ from rest_framework.authtoken.apps import AuthTokenConfig
 
 
 class UsersConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
     name = "users"
     verbose_name = _("User")
     verbose_name_plural = _("Users")
