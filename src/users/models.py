4b38c1de53ab5a64e93ba23ae4060adbac021c0a
Leandro de Souza <leandrodesouzadev@gmail.com>
2022-06-27 11:36:19 -0300

    Fix (i18n): replaces ugettext_lazy to gettext_lazy

--- src/users/models.py
+++ src/users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, Group
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from app.base.models import BaseModel
 
 
