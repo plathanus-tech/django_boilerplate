8936fa2a105aaf27911e33d6e614088eb9728f82
Leandro de Souza <leandrodesouzadev@gmail.com>
2022-09-20 14:00:53 -0300

    Fix (ext): Fallbacks to default `location` as 'media' if no `AWS_MEDIA_LOCATION` setting is available

--- src/app/ext/storage/aws_s3.py
+++ src/app/ext/storage/aws_s3.py
@@ -5,7 +5,7 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class PrivateMediaStorage(S3Boto3Storage):
     """Stores privately on Aws S3"""
 
-    location = settings.AWS_MEDIA_LOCATION
+    location = getattr(settings, "AWS_MEDIA_LOCATION", "media")
     default_acl = "private"
     file_overwrite = False
     custom_domain = False
@@ -14,7 +14,7 @@ class PrivateMediaStorage(S3Boto3Storage):
 class PublicMediaStorage(S3Boto3Storage):
     """Stores public on Aws S3"""
 
-    location = settings.AWS_MEDIA_LOCATION
+    location = getattr(settings, "AWS_MEDIA_LOCATION", "media")
     default_acl = "public-read"
     file_overwrite = False
     custom_domain = False
