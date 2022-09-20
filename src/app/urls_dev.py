641d13f7145d6d9ff4cb07a98c143a448d736c3f
Leandro de Souza <leandrodesouzadev@gmail.com>
2022-09-20 11:36:51 -0300

    Fix (docker): Adds some services that were missing on prod

--- src/app/urls_dev.py
+++ src/app/urls_dev.py
@@ -9,5 +9,5 @@ if settings.DEBUG:  # pragma: no cover
 
     urlpatterns += [
         path("__debug__/", include(debug_toolbar.urls)),
-        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
     ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
