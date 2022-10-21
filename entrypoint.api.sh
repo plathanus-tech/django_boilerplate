5b67b7d8b94806eefc5f0ccba099522c4ebd51dd
Leandro de Souza <leandrodesouzadev@gmail.com>
2022-10-21 09:16:45 -0300

    Fix (entrypoint): Removed the netcat await for postgres to come up on dev environment.

--- entrypoint.api.sh
+++ entrypoint.api.sh
@@ -1,19 +1,8 @@
 #!/bin/sh
 set -e
 
-if [ "$DJANGO_SETTINGS_MODULE" = "app.settings.dev" ]
-then
-
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-        echo "Waiting for postgres... $SQL_HOST $SQL_PORT"
-        sleep 5
-    done
-
-    echo "PostgreSQL started"
-fi
-
-python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py compilemessages -v 0
+python manage.py migrate
 
 exec "$@"
--- entrypoint.worker.sh
+++ entrypoint.worker.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 set -e
 
-if [ "$DJANGO_SETTINGS_MODULE" = "app.settings.dev" ]
-then
-
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-        echo "Waiting for postgres... $SQL_HOST $SQL_PORT"
-        sleep 5
-    done
-
-    echo "PostgreSQL started"
-fi
-
 python manage.py collectstatic --noinput
 python manage.py compilemessages -v 0
 
