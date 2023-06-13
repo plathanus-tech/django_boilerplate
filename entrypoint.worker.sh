c077adbe1398f87f4d8bd0104b26fa606390df5e
Leandro de Souza <leandrodesouzadev@gmail.com>
2023-06-13 14:55:06 -0300

    Infra: Moves dockerfiles to the infra folder, updates volumes on dev env

--- entrypoint.worker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-python manage.py collectstatic --noinput
-python manage.py compilemessages -v 0
-
-exec "$@"
---
+++ infra/traefik/traefik.Dockerfile
@@ -0,0 +1,3 @@
+FROM traefik:v2.4
+
+COPY infra/traefik/traefik.yml /etc/traefik/
