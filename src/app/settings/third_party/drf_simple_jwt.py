93f1b5bfe92251a01edaccffa5cf4230546c2dac
Leandro de Souza <leandrodesouzadev@gmail.com>
2023-11-17 10:53:44 -0300

    Fix (conf): Removes update last login from the drf-simple-jwt serializers, we take care of updating them

--- src/app/settings/third_party/drf_simple_jwt.py
+++ src/app/settings/third_party/drf_simple_jwt.py
@@ -10,6 +10,8 @@ REFRESH_TOKEN_LIFETIME_DAYS = env.int("REFRESH_TOKEN_LIFETIME_DAYS", default=7)
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=ACCESS_TOKEN_LIFETIME_MINUTES),
     "REFRESH_TOKEN_LIFETIME": timedelta(REFRESH_TOKEN_LIFETIME_DAYS),
-    "UPDATE_LAST_LOGIN": True,
+    "UPDATE_LAST_LOGIN": False,
+    # We don't want that this is controlled by the serializers of the drf-simple-jwt,
+    # we take care of it
     "ROTATE_REFRESH_TOKENS": True,
 }
